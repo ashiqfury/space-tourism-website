d4df0e73c571c72354a1bf294839ef86decf3385
Fury <ashiqasraf07@gmail.com>
2021-10-30 21:43:44 +0530

    Completed destination page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,9 +1,24 @@
 import Navbar from '../components/Navbar';
 import data from '../data';
 import moon from '../assets/destination/image-moon.png';
+import mars from '../assets/destination/image-mars.png';
+import europa from '../assets/destination/image-europa.png';
+import titan from '../assets/destination/image-titan.png';
+import { useEffect, useState } from 'react';
+
+const images = [moon, mars, europa, titan];
 
 const Destination = () => {
-	console.log(data.destinations[0].images.png);
+	const [index, setIndex] = useState(0);
+
+	useEffect(() => {
+		const list = document.querySelectorAll('li.planet-link');
+		for (let i = 0; i < list.length; i++) {
+			list[i].classList.remove('active');
+			i === index && list[i].classList.add('active');
+		}
+	}, [index]);
+
 	return (
 		<div className="destination">
 			<Navbar />
@@ -13,15 +28,36 @@ const Destination = () => {
 				</h5>
 				<div className="planet-container">
 					<div className="planet-img-wrapper">
-						<img src={moon} alt="Planets" />
+						<img src={images[index]} alt="Planets" />
 					</div>
 					<div className="planet-text-wrapper">
 						<ul className="planet-links">
-							<li className="planet-link">Moon</li>
-							<li className="planet-link">Mars</li>
-							<li className="planet-link">Europa</li>
-							<li className="planet-link">Titan</li>
+							<li className="planet-link active" onClick={() => setIndex(0)}>
+								Moon
+							</li>
+							<li className="planet-link" onClick={() => setIndex(1)}>
+								Mars
+							</li>
+							<li className="planet-link" onClick={() => setIndex(2)}>
+								Europa
+							</li>
+							<li className="planet-link" onClick={() => setIndex(3)}>
+								Titan
+							</li>
 						</ul>
+						<h1 className="planet--name">{data.destinations[index].name}</h1>
+						<p className="planet--description">{data.destinations[index].description}</p>
+						<div className="planet--divider"></div>
+						<div className="planet--stats-container">
+							<div className="planet--stats">
+								<p className="heading">Avg. Distance</p>
+								<h4 className="value">{data.destinations[index].distance}</h4>
+							</div>
+							<div className="planet--stats">
+								<p className="heading">Est. Travel Time</p>
+								<h4 className="value">{data.destinations[index].travel}</h4>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>

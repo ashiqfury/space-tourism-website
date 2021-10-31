a101854f27e903734ec145372edfa02ab600da57
Fury <ashiqasraf07@gmail.com>
2021-10-31 13:32:19 +0530

    Completed navbar functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import Technology from './pages/Technology';
 import Crew from './pages/Crew';
 import Home from './pages/Home';
 import './scss/style.scss';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			{/* <Home /> */}
-			{/* <Destination /> */}
-			{/* <Crew /> */}
-			<Technology />
-		</div>
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<Redirect to="/home" />
+				</Route>
+				{/* <Route to="/space-tourism-website">
+					<Redirect to="/home" />
+				</Route> */}
+				<Route path="/home">
+					<Home />
+				</Route>
+				<Route path="/destination">
+					<Destination />
+				</Route>
+				<Route path="/crew">
+					<Crew />
+				</Route>
+				<Route path="/technology">
+					<Technology />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
+
 const Navbar = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		const path = location.pathname.split('/')[1];
+		const element = document.getElementById(`${path}`);
+		element.classList.add('active');
+	}, [location.pathname]);
+
 	return (
 		<div className="navbar">
 			<div className="logo"></div>
@@ -6,22 +17,22 @@ const Navbar = () => {
 				<div className="bg-blur"></div>
 				<ul className="nav-links">
 					<li className="navlink">
-						<a href="/" className="active">
+						<a href="/" id="home">
 							<strong>00</strong> Home
 						</a>
 					</li>
 					<li className="navlink">
-						<a href="/destination">
+						<a href="/destination" id="destination">
 							<strong>01</strong> Destination
 						</a>
 					</li>
 					<li className="navlink">
-						<a href="/crew">
+						<a href="/crew" id="crew">
 							<strong>02</strong> Crew
 						</a>
 					</li>
 					<li className="navlink">
-						<a href="/technology">
+						<a href="/technology" id="technology">
 							<strong>03</strong> Technology
 						</a>
 					</li>
